27dafd48b8d4d60717b9523b8184257f47490ed8
Olivier Pelletier <shooter994@gmail.com>
2020-07-12 21:20:52 -0400

    Env variable round 2

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 let config;
+if(process.env.NODE_ENV !== 'dev'){
   config = {
     api: "https://ninjifyweb.azurewebsites.net/"
+  }
+} else {
+  config = {
+    api: process.env.URL
+  }
 }
 
 export { config }
